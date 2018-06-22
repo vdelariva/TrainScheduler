8e98679c60dd9846c98e7e0f2a1cce73b80dca12
Vger <vcdelariva@gmail.com>
2018-06-22 16:16:09 -0500

    Basic functionality

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -10,10 +10,11 @@ messagingSenderId: "157111617944"
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
-var database = firebase.database();
+// const database = firebase.database().ref().child("schedule");
+const database = firebase.database();
 
 // Initial Values
-var objdata = {
+var trainData = {
 
     trainName: "",
     destination: "",
@@ -28,34 +29,69 @@ var objdata = {
 $("#submit").on("click", function(event) {
     event.preventDefault();
     console.log($("trainName").val())
-    objdata.trainName = $("#trainName").val().trim();
-    objdata.destination = $("#destination").val().trim();
-    objdata.trainTime= $("#trainTime").val().trim();
-    objdata.frequency = $("#frequency").val().trim();
+    trainData.trainName = $("#trainName").val().trim();
+    trainData.destination = $("#destination").val().trim();
+    trainData.trainTime= $("#trainTime").val().trim();
+    trainData.frequency = $("#frequency").val().trim();
  
     database.ref().push({
-        trainName: objdata.trainName,
-        destination: objdata.destination,
-        trainTime: objdata.trainTime,
-        frequency: objdata.frequency
+        trainName: trainData.trainName,
+        destination: trainData.destination,
+        trainTime: trainData.trainTime,
+        frequency: trainData.frequency
     })
 });
 
 // When item added to database
 database.ref().on("child_added", function(snapshot){       
 
-    // var monthsWorked = moment().diff(moment(StartD), "months")
-    // var monthlyR = parseInt(childsnapshot.val().MonthlyRate)
+    // Get the first train time, convert time format
+    var time = moment(snapshot.val().trainTime,"HH:mm")
 
-    $("#table > tbody").append("<tr> <td>" 
-    + "<i class='fas fa-train' style='color:#619B83'></i>"
-    + " " + snapshot.val().trainName + "</td>" 
+    console.log("Name: "+snapshot.val().trainName)
+    console.log("trainTime: "+ moment(time).format("HH:mm"))
+    console.log("Time"+time);
+    console.log("now: "+moment().format("HH:mm"));
+
+    // Calculate the time difference between now and the first train time
+    var trainDiff = moment().diff(moment(time),"minutes");
+    console.log("diff: "+trainDiff)
+
+    // Use modulus operater to calculate the minutes to next train
+    var remainder = trainDiff % snapshot.val().frequency
+    console.log("remainder: "+remainder)
+
+    var minToArrival;
+    // If the first train of the day has not arrived, the remainder is the time for the next train arrival
+    if (trainDiff < 0) {
+        minToArrival = Math.abs(remainder);
+    }
+    // Otherwise, calculate arrival time by subtracting the remainder from the frequency
+    else {
+        minToArrival = snapshot.val().frequency - remainder;
+    }
+    console.log("Arrival: "+minToArrival)
+
+    $("#schedule > tbody").append("<tr>" 
+    + "<th scope='row'><i class='fas fa-train' style='color:#619B83'></i></th>"
+    + "<td>" + snapshot.val().trainName + "</td>" 
     + "<td>" + snapshot.val().destination + "</td>" 
     + "<td>" + parseInt(snapshot.val().frequency) + "</td>" 
-    + "<td>" + "09:00" + "</td>"
-    + "<td>" + "45" +  "</td> </tr>")
+    + "<td>" + moment().add(minToArrival,"minutes").format("HH:mm") + "</td>"
+    + "<td>" + minToArrival +  "</td>"
+    + "<td><i class='far fa-edit edit'></i></td>"
+    + "<td><i class='far fa-trash-alt trash'></i></tr>")
 
 });
+
+$(document).on("click",".edit", function(event) {
+    console.log("edit");
+});
+
+$(document).on("click",".trash", function(event) {
+    console.log("trash");
+});
+
 
 // <span class="fa-stack fa-2x">
 //   <i class="fas fa-camera fa-stack-1x"></i>
